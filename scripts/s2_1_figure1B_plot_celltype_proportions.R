4fedad57d6ab81e0e3b96f3f91fb73e5bbc5b6d7
Pennitz Peter <73164857+Peetterp@users.noreply.github.com>
2022-02-25 14:46:43 +0100

    Update s2_1_figure1B_plot_celltype_proportions.R

--- scripts/s2_1_figure1B_plot_celltype_proportions.R
+++ scripts/s2_1_figure1B_plot_celltype_proportions.R
@@ -1,63 +1,25 @@
-#' ---
-#' output:
-#'   html_document:
-#'     toc: true
-#'     toc_float: true
-#'     code_folding: show
-#' ---
-#'
-
-# # INITIATE SCRIPT ----
-rm(list = ls())
-
-
-r_on_server = F
-
-if(r_on_server) {
-  rootpath = "/net/ifs1/san_projekte/projekte/"
-  .libPaths("/net/ifs1/san_projekte/projekte/genstat/07_programme/rpackages/forostar")
-  toolboxH::initializeSkript(computer = "forostar" )
-} else {
-  rootpath =  "R:"
-  toolboxH::initializeSkript(computer = "local" )
-}
-require(toolboxH) # https://github.com/holgerman/toolboxH
-knitr::opts_chunk$set(cache = F, results = "markup", echo=T, fig.width = 10, fig.height = 7 )
-
-packages2load = c("magrittr","knitr", "ggplot2", "scales", "ggthemes", "assertr","Seurat", 'paletteer',"pheatmap", "cowplot", "here", "plotly", "ggrepel", "ggforce", "dplyr")
-
-new.packages <- packages2load[!(packages2load %in% installed.packages()[,"Package"])]
-if(length(new.packages)) BiocManager::install(new.packages)
-
-for(i in packages2load) {
-
-  suppressPackageStartupMessages(library(i, character.only = TRUE))
-
-}
-
-
 # CREDITS to https://stackoverflow.com/questions/48184645/how-can-i-put-the-labels-outside-of-piechart
 
 # # LOAD DATA ----
 #
-lunge_merged = readRDS(here("results/s1_3_lunge_merged.RDS")) # processed integrated data, available at https://www.health-atlas.de/studies/54
+Errint <- readRDS(".../Data/s603_2_seurat_lunge_merged_21-12-11.RDS") # processed integrated data, available at https://www.health-atlas.de/studies/54
 
 
 # # CREATE DATA ----
 
-lunge_merged.meta.data = data.table(lunge_merged@meta.data, keep.rownames = T)
+Errint.meta.data = data.table(Errint@meta.data, keep.rownames = T)
 
-tissue2groupm_counted = lunge_merged.meta.data[,.N, .(cluster_seurat_v2,  species)]
+tissue2groupm_counted = Errint.meta.data[,.N, .(celltype,  species)]
 
 
-# tissue2groupm_counted[, cluster_seurat_v2:= factor(cluster_seurat_v2, levels = reihenfolge)]
+ tissue2groupm_counted[, celltype:= factor(celltype, levels = reihenfolge)]
 
 tissue2groupm_counted[,prozi := N/sum(N), species]
 tissue2groupm_counted
 
 
 
-setorder(tissue2groupm_counted, species,cluster_seurat_v2)
+setorder(tissue2groupm_counted, species,celltype)
 tissue2groupm_counted2 <- tissue2groupm_counted %>% group_by(species) %>%
   mutate(end = 2 * pi * cumsum(N)/sum(N),
          start = lag(end, default = 0),
@@ -68,23 +30,21 @@ tissue2groupm_counted2 <- tissue2groupm_counted %>% group_by(species) %>%
 tissue2groupm_counted2 = data.table(tissue2groupm_counted2)
 
 
-levels(tissue2groupm_counted2$cluster_seurat_v2)
+levels(tissue2groupm_counted2$celltype)
 
 # tissue2groupm_counted2[,species:= factor(species, levels = c('Hamster', "Mouse" , "HumanK", "HumanT"))]
 
-tissue2groupm_counted2[,cluster_seurat_v22 :=  str_replace_all(cluster_seurat_v2, "Proliferating", "Prolif.") %>% str_wrap(., width = 16)]
-setorder(tissue2groupm_counted, cluster_seurat_v2)
-tissue2groupm_counted2[,cluster_seurat_v22 := factor(cluster_seurat_v22, levels = unique(cluster_seurat_v22))]
+tissue2groupm_counted2[,celltype1 :=  str_replace_all(celltype, "Proliferating", "Prolif.") %>% str_wrap(., width = 16)]
+setorder(tissue2groupm_counted, celltype)
+tissue2groupm_counted2[,celltype1 := factor(celltype1, levels = unique(celltype1))]
 
 
 
 # # PLOT ----
-nogpalette3 = c("#CB769E", "#DE639A", "#A85C85", "#7C6A0A", "#4F6D7A", "#368F8B",
-                "#246A73", "#5CC1BC", "#62C370", "#F7C548", "#F97E44", "#FB3640",
-                "#B7245C", "#0D3B66", "#3E2F5B", "#B2675E", "#644536")
+nogpalette3 = c("#CB769E", "#DE639A", "#A85C85", "#0081AF", "#4F6D7A", "#7C6A0A", "#368F8B", "#246A73", "#5CC1BC", "#62C370", "#F7C548", "#F97E44", "#FB3640", "#B7245C", "#0D3B66", "#3E2F5B", "#B2675E", "#644536")
 
 
-pieplot = ggplot(tissue2groupm_counted2,aes( fill = cluster_seurat_v22, color = cluster_seurat_v22)) +
+pieplot = ggplot(tissue2groupm_counted2,aes( fill = celltype1, color = celltype1)) +
   geom_arc_bar(aes(x0 = 0, y0 = 0, r0 = 0, r = 1,
                    start = start, end = end)
                # , col = "white"
@@ -93,7 +53,7 @@ pieplot = ggplot(tissue2groupm_counted2,aes( fill = cluster_seurat_v22, color = 
   coord_fixed()  +
   # geom_text(aes(x = 1.05 * sin(middle), y = 1.05 * cos(middle), label = cluster_seurat_v2,
   # hjust = hjust, vjust = vjust),size=2)+ #,min.segment.length=0.1, force_pull = 0.1000) +
-
+  
   scale_x_continuous(limits = c(-1.01, 1.01),  # Adjust so labels are not cut off
                      name = "", breaks = NULL, labels = NULL) +
   scale_y_continuous(limits = c(-1.01, 1.01),      # Adjust so labels are not cut off
@@ -111,9 +71,6 @@ pieplot = ggplot(tissue2groupm_counted2,aes( fill = cluster_seurat_v22, color = 
   scale_fill_manual(values = nogpalette3)
 
 pieplot
-
-jpeg(here("results/s2_1_figure1B_plot_celltype_proportions.jpeg"),width = 18,height = 5,units = "in", quality = 100, res = 150)
-print(pieplot)
 dev.off()
 
 # # FINALIZE SCRIPT----
